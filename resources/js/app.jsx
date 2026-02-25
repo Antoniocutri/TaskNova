63bb928563859cecf5f43282b6823a0ae13ff575
Antonio <antoniocutri841@gmail.com>
2026-02-25 15:18:57 +0100

    installed react

---
+++ resources/js/app.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+function App() {
+    return <h1>Ciao da React + Laravel 🚀</h1>;
+}
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
