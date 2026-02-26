385fd244ee8abf8e480e0951e30f44dcb1b57e46
Antonio <antoniocutri841@gmail.com>
2026-02-26 15:44:22 +0100

    created layout, navbar and homepage component

---
+++ resources/js/views/Homepage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+ function Homepage() {
+    return(
+        <button class="btn btn-secondary">Homepage</button>
+    )
+}
+
+export default Homepage;
