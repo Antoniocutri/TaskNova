f8a8acf627749d65b2131b591a47695170b04a67
Antonio <antoniocutri841@gmail.com>
2026-03-01 18:25:44 +0100

    add logot button

--- resources/js/views/Homepage.jsx
+++ resources/js/views/Homepage.jsx
@@ -1,16 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import routes from "../router/routes";
 
 
 
 function Homepage() {
+    const {
+        handleSubmit,
+    } = useForm({});
+
     const navigate = useNavigate()
+
+    const onSubmit = ()=>{
+        console.log('cisojhsfii')
+    };
+
     return(
         <>
-            <button className="btn btn-secondary" onClick={() => navigate(routes.login)}>Homepage</button>
+            <button className="btn btn-primary" onClick={() => navigate(routes.login)}>Homepage</button>
             <button className="btn btn-secondary" onClick={() => navigate(routes.register)}>registarti</button>
+
+            <form className="btn btn-error" onSubmit={handleSubmit(onSubmit)}>Logout</form>
         </>
     )
 }
