040898d756d2923791124d1a35a09ae5acd26629
Antonio <antoniocutri841@gmail.com>
2026-03-01 17:23:52 +0100

    updated fortify service provider

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -29,6 +29,14 @@ class FortifyServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Fortify::loginView(function () {
+            return view('welcome');
+        });
+
+        Fortify::registerView(function () {
+            return view('welcome');
+        });
+
         Fortify::createUsersUsing(CreateNewUser::class);
         Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
         Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
