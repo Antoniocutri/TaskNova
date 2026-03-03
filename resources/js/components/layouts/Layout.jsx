f1ed38ddd0d5a47de0408f6be5125ea3128d2bd3
Antonio <antoniocutri841@gmail.com>
2026-03-03 22:46:00 +0100

    add navbar and footer component to layout

--- resources/js/components/layouts/Layout.jsx
+++ resources/js/components/layouts/Layout.jsx
@@ -1,7 +1,14 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../layoutComponents/Navbar";
+import Footer from "../layoutComponents/Footer";
 
 export default function Layout(){
     return (
-        <Outlet/>
+        <>
+            <Navbar/>
+            <Outlet/>  
+            <Footer/>
+        </>
+
     )
 }
