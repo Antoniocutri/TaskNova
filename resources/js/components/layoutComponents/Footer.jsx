83bd5de395ebc81927fcfdc9d2d2cb65979430dd
Antonio <antoniocutri841@gmail.com>
2026-03-03 22:45:36 +0100

    created footer component

---
+++ resources/js/components/layoutComponents/Footer.jsx
@@ -0,0 +1,30 @@
+import { 
+  FaFacebook, 
+  FaInstagram, 
+  FaLinkedin, 
+  FaYoutube, 
+  FaTiktok,
+  FaDiscord,
+} from "react-icons/fa";
+
+export default function Footer() {
+    return(
+        <>
+            <footer className="footer footer-horizontal footer-center text-white rounded p-5 back-primary">
+                <aside>
+                    <div className="grid grid-flow-col gap-4">
+                        <FaFacebook className="text-blue-900 text-2xl"/>
+                        <FaInstagram className="text-pink-400 text-2xl"/>
+                        <FaLinkedin className="text-blue-900 text-2xl"/>
+                        <FaYoutube className="text-red-600 text-2xl"/>
+                        <FaTiktok className="text-black text-2xl"/>
+                        <FaDiscord className="text-indigo-900 text-2xl"/>
+                    </div>
+                </aside>
+                <aside>
+                    <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
+                </aside>
+            </footer>
+        </>
+    )
+}
