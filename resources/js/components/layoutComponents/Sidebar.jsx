3f07919fce1411d7e898ee37da7e361683098f76
Antonio <antoniocutri841@gmail.com>
2026-03-03 23:16:56 +0100

    created sidebar component and used in layout

---
+++ resources/js/components/layoutComponents/Sidebar.jsx
@@ -0,0 +1,83 @@
+import { Link, useLocation, useNavigate } from "react-router-dom"
+import { LayoutDashboard, ListTodo, PlusCircle, LogOut } from "lucide-react"
+import { UserContext } from "../../context/UserContext"
+import { useForm } from "react-hook-form";
+import { useContext } from "react";
+
+export default function Sidebar() {
+    const {
+        handleSubmit,
+    } = useForm({});
+
+    const location = useLocation()
+    const navigate = useNavigate()
+
+    const {signOut} = useContext(UserContext)
+
+    const handleLogout = async () =>{
+        await signOut();
+        navigate(routes.login);
+    };
+
+    const menuItems = [
+        { name: "Dashboard", path: "/", icon: <LayoutDashboard size={18} /> },
+        { name: "Tasks", path: "/tasks", icon: <ListTodo size={18} /> },
+    ]
+
+    return (
+        <div className="hidden md:flex flex-col justify-between h-screen w-64 bg-slate-900 text-slate-200 shadow-lg">
+
+            {/* Top Section */}
+            <div>
+      
+
+                {/* Menu */}
+                <ul className="mt-4 px-3 space-y-1">
+                    {menuItems.map((item, index) => {
+                        const isActive = location.pathname === item.path
+
+                        return (
+                            <li key={index}>
+                                <Link
+                                    to={item.path}
+                                    className={`
+                                        flex items-center gap-3 px-4 py-2 rounded-lg text-sm font-medium transition-all
+                                        ${isActive
+                                            ? "bg-slate-800 text-white"
+                                            : "text-slate-400 hover:bg-slate-800 hover:text-white"
+                                        }
+                                    `}
+                                >
+                                    {item.icon}
+                                    {item.name}
+                                </Link>
+                            </li>
+                            
+                        )
+                    })}
+                     <li key="newTask">
+                        <button
+                            className="flex items-center gap-3 w-full px-4 py-2 rounded-lg text-sm font-medium text-slate-400 hover:bg-slate-800 hover:text-white transition-all"
+                        >
+                            <PlusCircle size={18} />
+                            New Task
+                        </button>
+                    </li>
+                   
+                </ul>
+            </div>
+
+            {/* Bottom Section */}
+            <div className="p-4 border-t border-slate-800">
+                <form onSubmit={handleSubmit(handleLogout)}>
+                    <button
+                        className="flex items-center gap-3 w-full px-4 py-2 text-sm font-medium text-slate-400 hover:bg-red-500 hover:text-white rounded-lg transition-all"
+                    >
+                        <LogOut size={18} />
+                        Logout
+                    </button>
+                </form>
+            </div>
+        </div>
+    )
+}
--- resources/js/components/layouts/Layout.jsx
+++ resources/js/components/layouts/Layout.jsx
@@ -1,12 +1,22 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../layoutComponents/Navbar";
 import Footer from "../layoutComponents/Footer";
+import Sidebar from "../layoutComponents/Sidebar";
 
 export default function Layout(){
     return (
         <>
             <Navbar/>
-            <Outlet/>  
+
+            <section className="grid grid-cols-7 gap-4">
+                <div className="col-span-5 md:col-span-1">
+                    <Sidebar />
+                </div>
+                <div className="col-span-7 md:col-span-6">
+                    <Outlet />
+                </div>
+            </section>
+
             <Footer/>
         </>
 
