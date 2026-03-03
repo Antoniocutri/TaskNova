0692cf4838c9a1a1d610a4d9ebb055b20ee889b5
Antonio <antoniocutri841@gmail.com>
2026-03-03 23:16:29 +0100

    updated homepage

--- resources/js/views/Homepage.jsx
+++ resources/js/views/Homepage.jsx
@@ -1,41 +1,11 @@
 import React, { useContext } from "react";
 import ReactDOM from "react-dom/client";
-import { useForm } from "react-hook-form";
-import { useNavigate } from "react-router-dom";
-import routes from "../router/routes";
-import { UserContext } from "../context/UserContext";
-
-
 
 function Homepage() {
-    const {
-        handleSubmit,
-    } = useForm({});
-
-    const navigate = useNavigate()
-
-    const {user, signOut} = useContext(UserContext)
-
-    const handleLogout = async () =>{
-
-        await signOut();
-        navigate(routes.login);
-    };
 
     return(
         <>
-            {(!user && (
-                <> 
-                    <button className="btn btn-primary" onClick={() => navigate(routes.login)}>Homepage</button>
-                    <button className="btn btn-secondary" onClick={() => navigate(routes.register)}>registarti</button>
-                </>
-            )) || (
-                <>
-                    <form  onSubmit={handleSubmit(handleLogout)}>
-                        <button className="btn btn-error">Logout</button>
-                    </form>
-                </>
-            )}
+
         </>
     )
 }
