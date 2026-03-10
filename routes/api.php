805bf7f85306070a08d4267b2235ee3046570b0e
Antonio <antoniocutri841@gmail.com>
2026-03-10 23:45:12 +0100

    moved dashboard route in api route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -11,5 +12,7 @@ Route::middleware('auth:sanctum')->group(function () {
     });
 
     Route::apiResource('tasks', TaskController::class);
+
+    Route::get('/dashboard', [DashboardController::class, 'index']);
 
 });
