f35706b64c36b28e1e2d68fcb40d46248ad89f7b
Antonio <antoniocutri841@gmail.com>
2026-03-14 15:10:43 +0100

    removed border on pie chart focus

--- resources/js/components/DashboardComponents/PieChart.jsx
+++ resources/js/components/DashboardComponents/PieChart.jsx
@@ -3,9 +3,14 @@ import ReactApexChart from "react-apexcharts";
 
 export default function PieChart({percentage}) {
 
+    const series = [
+        percentage?.pending ?? 0,
+        percentage?.completed ?? 0,
+        percentage?.overdue ?? 0
+    ];
+
     const [state, setState] = useState({
         
-        series: [percentage?.pending, percentage?.completed,  percentage?.overdue ],
         options: {
             chart: {
                 width: 380,
@@ -29,13 +34,12 @@ export default function PieChart({percentage}) {
             }]
         },
         
-        
     });
 
     return (
         <div>
-            <div id="chart card bg-base-100 border border-base-200 shadow-sm p-5 m-5 ">
-                <ReactApexChart options={state.options} series={state.series} type="pie" width="100%" />
+            <div id="chart" className="card bg-base-100 border border-base-200 shadow-sm p-5 m-5">
+                <ReactApexChart options={state.options} series={series} type="pie" width="100%" />
             </div>
             <div id="html-dist"></div>
         </div>
