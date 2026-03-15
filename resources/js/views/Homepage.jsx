824cbe7258c70261d8305d4f8e0a726f79626915
Antonio <antoniocutri841@gmail.com>
2026-03-15 18:35:54 +0100

    used dueSoonTask component in Homepage and updated homepage style

--- resources/js/views/Homepage.jsx
+++ resources/js/views/Homepage.jsx
@@ -1,7 +1,8 @@
 import { useLoaderData } from "react-router-dom";
 import Header from "../components/DashboardComponents/Header";
 import PieChart from "../components/DashboardComponents/PieChart";
-import { FaChartPie } from "react-icons/fa6";
+import { FaChartPie, FaClock } from "react-icons/fa6";
+import DueSoonTasks from "../components/DashboardComponents/DueSoonTasks";
 
 function Homepage() {
 
@@ -13,14 +14,33 @@ function Homepage() {
             <header className="m-5">
                 <Header stats={data?.data} />
             </header>
+           
+           <div className="card bg-base-100 shadow m-5 mb-10">
+                <div className="card-body">
 
-            <div className="w-2/4 mx-auto my-8 text-center text-3xl">
-                <h2 className=" font-semibold mb-4 flex items-center justify-center gap-2">
-                    <FaChartPie className="text-primary" />
-                    Distribuzione dei task
-                </h2>
-                <PieChart percentage={data?.data.percentages}/>
-            </div>
+                    <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 my-8">
+
+                        <div className="text-center">
+                            <h2 className="font-semibold mb-4 flex items-center justify-center gap-2 text-2xl">
+                                <FaChartPie className="text-primary" />
+                                Distribuzione dei task
+                            </h2>
+
+                            <PieChart percentage={data?.data.percentages}/>
+                        </div>
+
+                        <div className="text-center">
+                            <h2 className="font-semibold mb-4 flex items-center justify-center gap-2 text-2xl">
+                                <FaClock className="text-warning" /> Task in scadenza a breve
+                            </h2>
+
+                            <DueSoonTasks tasks={data?.data.due_soon}/>
+                        </div>
+
+                    </div> 
+
+                </div>
+           </div> 
         </>
     )
 }
